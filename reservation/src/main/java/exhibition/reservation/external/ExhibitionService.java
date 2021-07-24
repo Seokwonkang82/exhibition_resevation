6b364762bdec84591a1b987cfe69885ef11ea802
Seokwonkang82 <86943781+Seokwonkang82@users.noreply.github.com>
2021-07-24 20:33:14 +0900

    Update ExhibitionService.java

--- reservation/src/main/java/exhibition/reservation/external/ExhibitionService.java
+++ reservation/src/main/java/exhibition/reservation/external/ExhibitionService.java
@@ -9,10 +9,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="exhibition", url="http://exhibition:8080")
+
+@FeignClient(name="exhibition", url="http://exhibition:8082")
 public interface ExhibitionService {
-    @RequestMapping(method= RequestMethod.GET, path="/exhibitions")
-    public void getStatus(@RequestBody Exhibition exhibition);
+
+    //@RequestMapping(method= RequestMethod.GET, path="/exhibitions")
+    //public void getStatus(@RequestBody Exhibition exhibition);
+
+    @RequestMapping(method= RequestMethod.GET, value="/exhibitions/{id}", consumes = "application/json")
+    public Exhibition getstatus(@PathVariable("id") Long id);
+
 
 }
+
+
+
+//@FeignClient(name="resort", url="${feign.resort.url}")
+//public interface ResortService {
+
+    //@PatchMapping(path="/resorts/{id}")
+    //@RequestMapping(method= RequestMethod.PATCH, path="/resorts/{id}")
+    //public void resortStatusChange(@RequestParam("id") Long id, @RequestBody Resort resort);
+
+   // @RequestMapping(method= RequestMethod.GET, value="/resorts/{id}", consumes = "application/json")
+   // public Resort getResortStatus(@PathVariable("id") Long id);
+
 
